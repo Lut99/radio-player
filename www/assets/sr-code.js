130e0e6bf9f40d615b91f4e4669e8916239ec121
Lut99 <mullertim99@gmail.com>
2024-07-19 00:40:44 +0200

    Added the basics of SIMULATORRADIO player
    In particular, can start/stop and title, artits and artwork on website are updated.
    
    Next: re-adding notifications (with button), radio host and history. And sessions.

---
+++ www/assets/sr-code.js
@@ -0,0 +1,153 @@
+
+// radio = new Audio();
+// radio.src = "https://simulatorradio.stream/stream.mp3";
+
+var audio = null;
+var last = null;
+var show_notifs = false;
+
+function run_listener() {
+    // Check whether notification permissions have already been granted;
+    // if so, create a notification
+    const sock = new WebSocket("wss://ws.simulatorradio.com/");
+    sock.addEventListener("open", (event) => {
+        console.log("Opened le socket ", event.data);
+    });
+    sock.addEventListener("message", (event) => {
+        // Deserialize the message
+        data = JSON.parse(event.data);
+        if (data.type === "nowPlaying") {
+            // Show the notification
+            if (last === null || last != data.now_playing.title + "-" + data.now_playing.artist) {
+                const notification = new Notification(data.now_playing.title, {
+                    body: data.now_playing.artists + " - DJ " + data.djs.now.displayname,
+                    icon: data.now_playing.art,
+                    image: data.now_playing.art
+                });
+                last = data.now_playing.title + "-" + data.now_playing.artist;
+            }
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Setup the hoverer
+    document.getElementById("player-art-click").onmouseenter = function() {
+        document.getElementById("player-art-click").style.opacity = "1";
+    };
+    document.getElementById("player-art-click").onmouseleave = function() {
+        if (audio !== null) {
+            document.getElementById("player-art-click").style.opacity = "0";
+        }
+    };
+
+    // Setup the stream
+    const sock = new WebSocket("wss://ws.simulatorradio.com/");
+    sock.addEventListener("open", (event) => {
+        console.log("Connected to 'ws.simulatorradio.com' for currently playing information");
+    });
+    sock.addEventListener("message", (event) => {
+        // Deserialize the message
+        data = JSON.parse(event.data);
+        if (data.type === "nowPlaying") {
+            console.log("Received 'now-playing' update (" + data.now_playing.title + ", " + data.now_playing.artists + ", " + data.now_playing.art + ")");
+
+            // Only need  to do stuff when an update occurred
+            if (last === null || last != data.now_playing.title + "-" + data.now_playing.artist) {
+                // const notification = new Notification(data.now_playing.title, {
+                //     body: data.now_playing.artists + " - DJ " + data.djs.now.displayname,
+                //     icon: data.now_playing.art,
+                //     image: data.now_playing.art
+                // });
+                last = data.now_playing.title + "-" + data.now_playing.artist;
+                console.log("Received 'now-playing' update (" + data.now_playing.title + ", " + data.now_playing.artists + ", " + data.now_playing.art + ")");
+
+                // Update the site information
+                document.getElementById("player-title").innerText = data.now_playing.title;
+                document.getElementById("player-artist").innerText = data.now_playing.artists;
+                document.getElementById("player-art").setAttribute("src", data.now_playing.art);
+            }
+        }
+    });
+
+
+
+    // Get notification permission
+    // if ("Notification" in window) {
+    //     if (Notification.permission === "granted") {
+    //         console.log("Already have notif permission");
+    //         run_listener();
+    //     } else if (Notification.permission !== "denied") {
+    //         console.log("Getting notif permission");
+    //         // We need to ask the user for permission
+    //         Notification.requestPermission().then((permission) => {
+    //             // If the user accepts, let's create a notification
+    //             if (permission === "granted") {
+    //                 run_listener();
+    //             }
+    //         });
+    //     }
+
+        // audio = document.getElementById("radio")
+        // audio.play().then(() => {
+        //     if ("mediaSession" in navigator) {
+        //         navigator.mediaSession.playbackState = "playing";
+        //         navigator.mediaSession.metadata = new MediaMetadata({
+        //             title: "Unforgettable",
+        //             artist: "Nat King Cole",
+        //             album: "The Ultimate Collection (Remastered)",
+        //             artwork: [
+        //             {
+        //                 src: "https://dummyimage.com/96x96",
+        //                 sizes: "96x96",
+        //                 type: "image/png",
+        //             },
+        //             {
+        //                 src: "https://dummyimage.com/128x128",
+        //                 sizes: "128x128",
+        //                 type: "image/png",
+        //             },
+        //             {
+        //                 src: "https://dummyimage.com/192x192",
+        //                 sizes: "192x192",
+        //                 type: "image/png",
+        //             },
+        //             {
+        //                 src: "https://dummyimage.com/256x256",
+        //                 sizes: "256x256",
+        //                 type: "image/png",
+        //             },
+        //             {
+        //                 src: "https://dummyimage.com/384x384",
+        //                 sizes: "384x384",
+        //                 type: "image/png",
+        //             },
+        //             {
+        //                 src: "https://dummyimage.com/512x512",
+        //                 sizes: "512x512",
+        //                 type: "image/png",
+        //             },
+        //             ],
+        //         });
+        //     }
+        // }).catch((error) => {
+        //     console.error(error);
+        // });
+    // }
+});
+
+
+
+function play_sr() {
+    if (audio === null) {
+        audio = new Audio("https://simulatorradio.stream/stream.mp3");
+        audio.play();
+        document.getElementById("player-art-click").innerHTML = '<i class="fa-solid fa-pause"></i>';
+        console.log("Playing SIMULATORRADIO");
+    } else {
+        audio.pause();
+        audio = null;
+        document.getElementById("player-art-click").innerHTML = '<i class="fa-solid fa-play"></i>';
+        console.log("Pausing SIMULATORRADIO");
+    }
+}
